a5dd3880c9ea56e3ce33db95c5eca702f57b87ff
Miguel Lopez <miguel.lopez@kinesso.com>
2022-06-09 21:56:08 -0500

    Adding bootstrap repo

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, html
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+# app = Dash(__name__)
+app = Dash(
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
 app.title = 'Celsia Data Validator'
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Validator", href="#")),
+        dbc.NavItem(dbc.NavLink("Prediction", href="#")),
+        dbc.NavItem(dbc.NavLink("Statistics", href="#")),
+        dbc.NavItem(dbc.NavLink("User guide", href="#")),
+    ],
+    brand="Celsia",
+    brand_href="#",
+    color="light",
+    dark=False,
+)
 app.layout = html.Div([
-    html.H1('Hola mundo', style={'textAlign': 'center'})
+    navbar,
+    html.H1('Voltage Failure checker', style={'textAlign': 'center'}),
 
 ])
 
