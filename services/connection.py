fcc63c5a1912543695865816ce1876444be41915
Miguel Lopez <miguel.lopez@kinesso.com>
2022-07-02 16:51:06 -0500

    Uploading upload_clients service

--- services/connection.py
+++ services/connection.py
@@ -1,23 +1,32 @@
-import psycopg2
+import sqlalchemy
 import pandas as pd
 
-global connect
+global engine
 
 
 def start():
-    global connect
+    global engine
     name = "celsia"
     user = "team173"
     password = "4W5DBF0rT34m"
+    port = '5432'
 
     host = "validated-data.c0qqjwg62lhy.us-east-1.rds.amazonaws.com"
 
-    connect = psycopg2.connect(dbname=name, user=user, password=password, host=host)
+    url = 'postgresql+psycopg2://{user}:{password}@{host}:{port}/{name}'\
+        .format(name=name, user=user, password=password, port=port, host=host)
+    engine = sqlalchemy.create_engine(url)
 
 
 def get_clients():
-    global connect
+    global engine
 
-    return pd.read_sql_query("SELECT * FROM clients;", connect)
+    return pd.read_sql_query("SELECT * FROM clients;", engine)
+
+
+def upload_clients(client):
+    global engine
+
+    client.to_sql(name='clients', schema='public', con=engine, if_exists='append', index=False)
 
 
